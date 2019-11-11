4d1693842ddf80ff3e240961737f517077cbc82c
eir <m-laniakea@users.noreply.github.com>
2019-11-11 15:17:09 -0800

    constant/status: Prefix active-low statuses with 'Not'

--- stspin/constants/status.py
+++ stspin/constants/status.py
@@ -19,17 +19,17 @@ class MotorStatus:
 
 
 class Status:
-    HiZ: Final              = 0x0001
-    Busy: Final             = 0x0002
-    SwitchFlag: Final       = 0x0004  # low on closed switch, high on open
-    SwitchEvent: Final      = 0x0008  # high indicates falling edge detected
-    Dir: Final              = 0x0010
-    CmdNotPerformed: Final  = 0x0080
-    CmdWrong: Final         = 0x0100
-    Undervoltage: Final     = 0x0200  # active low
-    ThermalWarning: Final   = 0x0400  # active low
-    ThermalShutdown: Final  = 0x0800  # active low
-    Overcurrent: Final      = 0x1000  # active low
-    StepLossA: Final        = 0x2000  # low on stall detect
-    StepLossB: Final        = 0x4000  # low on stall detect
-    StepClockMode: Final    = 0x8000
+    HiZ: Final                  = 0x0001
+    NotBusy: Final              = 0x0002  # active low
+    SwitchFlag: Final           = 0x0004  # low on closed switch, high on open
+    SwitchEvent: Final          = 0x0008  # high on falling edge
+    Dir: Final                  = 0x0010
+    CmdNotPerformed: Final      = 0x0080
+    CmdWrong: Final             = 0x0100
+    NotUndervoltage: Final      = 0x0200  # active low
+    NotThermalWarning: Final    = 0x0400  # active low
+    NotThermalShutdown: Final   = 0x0800  # active low
+    NotOvercurrent: Final       = 0x1000  # active low
+    NotStepLossA: Final         = 0x2000  # low on stall detect
+    NotStepLossB: Final         = 0x4000  # low on stall detect
+    StepClockMode: Final        = 0x8000
