2b185f6b8ad04f4f7863b078ac17dcc19d2e126e
eir <m-laniakea@users.noreply.github.com>
2019-11-19 15:37:27 -0800

    setup.py: Specify description content type
    Fixes pypi being unable to process README

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(
       description='Interface for ST SpinFamily motor drivers',
       packages=find_packages(exclude=['tests']),
       long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       zip_safe=False)
