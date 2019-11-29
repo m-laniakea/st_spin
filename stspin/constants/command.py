e94a93ea257d63824f0ef4498361b95cdb62bd7e
eir <m-laniakea@users.noreply.github.com>
2019-11-29 09:55:36 -0800

    Command: Convert to type defined value

--- stspin/constants/command.py
+++ stspin/constants/command.py
@@ -1,5 +1,4 @@
 from typing import (
-    Dict,
     Optional,
 )
 from typing_extensions import (
@@ -24,54 +23,27 @@ class SpinCommand(SpinValue):
 
 
 class Command:
-    GoHome: Final           = 0x70
-    GoMark: Final           = 0x78
-    GoTo: Final             = 0x60
-    GoToDir: Final          = 0x68  # ORed with DIR
-    GoUntil: Final          = 0x82  # ORed with ACT, DIR
-    HiZHard: Final          = 0xA8
-    HiZSoft: Final          = 0xA0
-    Nop: Final              = 0x00
-    Move: Final             = 0x40  # ORed with DIR. Unuseable while running
-    ParamGet: Final         = 0x20  # ORed with target register value
-    ParamSet: Final         = 0x00  # ORed with target register value
-    ReleaseSw: Final        = 0x92  # ORed with ACT, DIR
-    ResetDevice: Final      = 0xC0
-    ResetPos: Final         = 0xD8  # Clears ABS_POS
-    Run: Final              = 0x50  # ORed with DIR
-    StatusGet: Final        = 0xD0
-    StepClock: Final        = 0x58  # ORed with DIR
-    StopHard:  Final        = 0xB8
-    StopSoft: Final         = 0xB0
+    GoHome: Final           = SpinCommand(0x70, 0)
+    GoMark: Final           = SpinCommand(0x78, 0)
+    GoTo: Final             = SpinCommand(0x60, 3)
+    GoToDir: Final          = SpinCommand(0x68, 3)  # ORed with DIR
+    GoUntil: Final          = SpinCommand(0x82, 3)  # ORed with ACT, DIR
+    HiZHard: Final          = SpinCommand(0xA8, 0)
+    HiZSoft: Final          = SpinCommand(0xA0, 0)
+    Nop: Final              = SpinCommand(0x00, 0)
 
-    @staticmethod
-    def getPayloadSize(command: int) -> int:
-        """return size of payload in bytes expected to follow command
+    # ORed with DIR. Unuseable while running
+    Move: Final             = SpinCommand(0x40, 3)
 
-        :command: command to query
-        :returns: payload size in bytes
-
-        """
-        assert(command in PayloadSize)
-
-        return PayloadSize[command]
-
-
-PayloadSize: Final[Dict[int, int]] = {
-    Command.GoHome:         0,
-    Command.GoMark:         0,
-    Command.GoTo:           3,
-    Command.GoToDir:        3,
-    Command.GoUntil:        3,
-    Command.HiZHard:        0,
-    Command.HiZSoft:        0,
-    Command.Nop:            0,
-    Command.Move:           3,
-    Command.ReleaseSw:      0,
-    Command.ResetDevice:    0,
-    Command.ResetPos:       0,
-    Command.Run:            3,
-    Command.StepClock:      0,
-    Command.StopHard:       0,
-    Command.StopSoft:       0,
-}
+    # ORed with target register value
+    ParamGet: Final         = SpinCommand(0x20, None)
+    # ORed with target register value
+    ParamSet: Final         = SpinCommand(0x00, None)
+    ReleaseSw: Final        = SpinCommand(0x92, 0)  # ORed with ACT, DIR
+    ResetDevice: Final      = SpinCommand(0xC0, 0)
+    ResetPos: Final         = SpinCommand(0xD8, 0)  # Clears ABS_POS
+    Run: Final              = SpinCommand(0x50, 3)  # ORed with DIR
+    StatusGet: Final        = SpinCommand(0xD0, None)
+    StepClock: Final        = SpinCommand(0x58, 0)  # ORed with DIR
+    StopHard:  Final        = SpinCommand(0xB8, 0)
+    StopSoft: Final         = SpinCommand(0xB0, 0)
